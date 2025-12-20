057f3d651cc642741f11c41e9107f48c67455594
mMoritzImendoerffer <moritz.imendoerffer@mailfence.com>
2025-12-20 13:13:00 +0100

    added TODO in startup.sh

--- init_project.sh
+++ init_project.sh
@@ -1,9 +1,11 @@
 conda create -n scrapy -f requirements.yml
 conda activate scrapy
-scrapy startproject ema_scrapy
+scrapy startproject ema_scraper
 
 mkdir -p storage
 
 # flatten the folder structure and use run_spider.py as entrypoint
-cd ema_scrapy && mv ema_scrapy/* . && rmdir ema_scrapy
+cd ema_scraper && mv ema_scraper/* . && rmdir ema_scraper
+
+# TODO: add startup for mongoDB
 
